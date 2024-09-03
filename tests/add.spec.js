af8cf265ad61fb16b15a35c88f6ec84a75cbc6c9
qiuwenhui <1059713136@qq.com>
2024-09-03 22:42:37 +0800

    ✨ feat: 匹配器：toEqual, toContain

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,13 +1,40 @@
 const add = require("../add.js");
 
-test("1 + 2 = 3", () => {
-  // 准备数据 -> given
-  const a = 1;
-  const b = 2;
+describe("Name of the group", () => {
+  test("1 + 2 = 3", () => {
+    // 准备数据 -> given
+    const a = 1;
+    const b = 2;
 
-  // 触发测试动作 -> when
-  const result = add(a, b);
+    // 触发测试动作 -> when
+    const result = add(a, b);
 
-  // 验证 -> then
-  expect(result).toBe(3);
+    // 验证 -> then
+    expect(result).toBe(3);
+  });
+
+  test("判断两个对象是否相等", () => {
+    const objA = {
+      name: "objA",
+    };
+
+    const objB = {
+      name: "objA",
+    };
+
+    expect(objA).toEqual(objB);
+  });
+
+  test("string contain str", () => {
+    const str = "string";
+    expect(str).toContain("str");
+    expect(str).not.toContain("hello");
+  });
+
+  // it 等价于 test
+  it("string contain str", () => {
+    const str = "string";
+    expect(str).toContain("str");
+    expect(str).not.toContain("hello");
+  });
 });
